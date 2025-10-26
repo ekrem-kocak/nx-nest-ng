93e5a5ba09385afc2c53f83aafcaf2c413c4d770
ekrem-kocak <ekrem-kocak@outlook.com>
2025-10-26 23:41:10 +0300

    Add NGXS and storage plugin.

--- apps/store-admin-portal/src/app/app.config.ts
+++ apps/store-admin-portal/src/app/app.config.ts
@@ -6,10 +6,20 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
+import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
+import { provideStore } from '@ngxs/store';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+
+    provideStore(
+      [],
+      withNgxsStoragePlugin({
+        keys: '*',
+      })
+    ),
   ],
 };
