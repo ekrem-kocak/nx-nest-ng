09893c7fbe3df8aa2b41dca69b3f338fed6ba914
ekrem-kocak <ekrem-kocak@outlook.com>
2025-10-29 23:15:45 +0300

    refactor: remove unused dashboard and main layout components

--- apps/store-admin-portal/src/app/app.routes.ts
+++ apps/store-admin-portal/src/app/app.routes.ts
@@ -1,29 +1,10 @@
 import { Route } from '@angular/router';
-import { MainLayoutComponent } from '../shared/components/main-layout/main-layout.component';
 
 export const appRoutes: Route[] = [
   {
     path: 'auth',
     loadChildren: () =>
       import('@nx-nest-ng/eku-store-feature-auth').then((m) => m.authRoutes),
-  },
-  {
-    path: '',
-    component: MainLayoutComponent,
-    children: [
-      {
-        path: 'dashboard',
-        loadComponent: () =>
-          import(
-            '../features/dashboard/pages/dashboard-page/dashboard-page.component'
-          ).then((m) => m.DashboardPageComponent),
-      },
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full',
-      },
-    ],
   },
   {
     path: '',
